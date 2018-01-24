793567d850e56370be463b3a00642d530829bbea
DIEGO <godie.mendoza@gmail.com>
2018-01-23 23:37:16 -0700

    content single and added functions

--- content.php
+++ content.php
@@ -6,7 +6,26 @@
 	<?php
 	printf( _nx( 'One Comment', '%1$s Comments', get_comments_number(), 'comments title', 'textdomain' ), number_format_i18n( 						get_comments_number() ) ); ?>
 </a>
+<?php if ( has_post_thumbnail() )  { ?>
+	<div class="row">
+		<div class="col s12 m7 l5">
+			<div class="card">
+			<div class="card-image">
+			<?php the_post_thumbnail('thumbnail'); ?>
+			<span class="card-title">TEST</span>
+			</div>
+		</div>
+		<div class="card-content">
+				<?php the_excerpt('Read the rest of this entry &raquo;'); ?>
+		</div>
+		<div class="card-action">
+			<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+		</div>
+	</div>
+</div>
+<?php } else { ?>
 
-	<div class="entry">
+	<div class="entry s12 m10 l7">
 		<?php the_excerpt('Read the rest of this entry &raquo;'); ?>
 	</div>
+<?php } ?>
--- functions.php
+++ functions.php
@@ -39,5 +39,62 @@ function materializegb_settings_page(){
 }
 
 
+function display_twitter_element()
+{
+	?>
+    	<input type="text" name="twitter_url" id="twitter_url" value="<?php echo get_option('twitter_url'); ?>" />
+    <?php
+}
+
+function display_facebook_element()
+{
+	?>
+    	<input type="text" name="facebook_url" id="facebook_url" value="<?php echo get_option('facebook_url'); ?>" />
+    <?php
+}
+
+function display_layout_element(){
+	?>
+		<input type="checkbox" name="theme_layout" value="1" <?php checked(1, get_option('theme_layout'), true); ?> />
+	<?php
+}
+
+function my_custom_post(){
+	register_post_type( 'info', array(
+		'labels' => array(
+			'name' => __( 'infopost' ),
+			'singular_name' => __( 'infopost'),
+		),
+		'public' => true,
+		'has_archive' => true,
+		'supports' => array(
+			'title',
+			'editor',
+			'thumbnail',
+			'custom-fields'
+		)
+	) );
+}
+
+add_action( 'init', 'my_custom_post');
+
+function display_theme_panel_fields()
+{
+	add_settings_section("section", "All Settings", null, "theme-options");
+	
+	add_settings_field("twitter_url", "Twitter Profile Url", "display_twitter_element", "theme-options", "section");
+    add_settings_field("facebook_url", "Facebook Profile Url", "display_facebook_element", "theme-options", "section");
+    add_settings_field( "theme_layout", "Do you want the layout to be responsive?", "display_layout_element", "theme-options", "section");
+
+    register_setting("section", "twitter_url");
+    register_setting("section", "facebook_url");
+    register_setting( "section", "theme_layout");
+}
+
+add_action("admin_init", "display_theme_panel_fields");
+
+
 add_theme_support( 'title-tag' );
+add_theme_support( 'post-thumbnails' );
+
 
